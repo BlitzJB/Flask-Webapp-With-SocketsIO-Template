f387f9163a01e0c7741e995b3628077fde6abea9
Joshua Bharathi <blitz04.dev@gmail.com>
2022-04-20 06:43:17 +0530

    using global request context

--- server/sockets.py
+++ server/sockets.py
@@ -1,12 +1,13 @@
 from .views import app
 
 from flask_socketio import SocketIO, Namespace, emit, join_room, leave_room
+from flask import request
 
 socket = SocketIO(app)
 
 class NameSpace(Namespace):
     def on_connect(self):
-        print('Connected')
+        print('Connected with ' + request.sid)
 
     def on_click(self, data): # on_<event_name>
         print('Clicked')
